24ff6ab5df7bc6a5738cd2431489a87771d18106
vitoriaBernard0-dev <vi.bernardo12@gmail.com>
2023-09-01 08:44:10 -0300

    js

--- script.php
+++
@@ -1,8 +0,0 @@
-<?php
-function toggleMenu() {
-    echo '<script>';
-    echo 'var menu = document.getElementById("menu");';
-    echo 'menu.classList.toggle("active");';
-    echo '</script>';
-}
-?>
