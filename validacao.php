62d8c0d3bcdcefb75496d5019ae28fb9c512175a
vitoriaBernard0-dev <vi.bernardo12@gmail.com>
2023-09-04 15:09:40 -0300

    commit vitoria

--- validacao.php
+++ validacao.php
@@ -9,11 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if($email===$usuario && $password===$senhacerta){
        $_SESSION["usuario_logado"] =  true;
-<<<<<<< HEAD
-       header("Location: index.php");
-=======
        header("Location: welcome.php");
->>>>>>> 377334d2f7424387fc85a0df2eee7c74e286ca16
     } else {
         echo "Usuário ou senha incorretos. Tente novamente.";
         header("Location: index.php");
