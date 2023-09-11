83ffa2880473dedeec9b9e96b9611b8a869b8cb5
vitoriaBernard0-dev <vi.bernardo12@gmail.com>
2023-09-11 09:02:19 -0300

    x

--- indexxaiomi.php
+++ indexxaiomi.php
@@ -33,7 +33,7 @@
         </nav>
     </header>
     <div id="banner">
-        <img id="banner" src="img/xiamoi.png" alt="">
+        <img id="banner" src="img/xaiomi.png" alt="">
     </div>
 
     <main id="product-list">
